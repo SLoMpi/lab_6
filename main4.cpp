f21ac2e7c73d244fd80637c3a5f9b3d82afe343a
SLoMpi <khodorovskiy1968@gmail.com>
2024-11-17 19:32:33 +0300

    Add files via upload

---
+++ main4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+map<string, int> uniqueWords(const string& str) {
+    map<string, int> wordCount;
+    string word;
+    for (char c : str) {
+        if (c == ' ' || c == ',' || c == '.') {
+            if (!word.empty()) {
+                wordCount[word]++;
+                word.clear();
+            }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        wordCount[word]++;
+    }
+    return wordCount;
+}
+int main() {
+    string str = "can you can";
+    map<string, int> words = uniqueWords(str);
+    cout << "Уникальные слова:" << endl;
+    for (const auto& pair : words) {
+        cout << pair.first << " ";
+    }
+}
